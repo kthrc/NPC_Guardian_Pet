95ea260f3ac5af7b0c3e409b4cd6de64ef3b7bc2
kthrc <106314022+kthrc@users.noreply.github.com>
2023-10-17 01:58:01 +0900

    231017

---
+++ Assets/Scripts/Setting.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Settings : MonoBehaviour
+{
+    public GameObject SettingPop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        // Go To NextScene(for clickObject)
+
+        GameObject clickObject = EventSystem.current.currentSelectedGameObject;
+        if (clickObject.name == "SettingBtn") //
+        {
+            //SceneManager.LoadScene(""); //
+
+            SettingPop.SetActive(true);
+        }
+    }
+}
